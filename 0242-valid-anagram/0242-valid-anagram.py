90a45e77ff1a20e504b0707f9a0d703585c4f55c
Al Shahriar Bin Shaheen <155906469+shahriarshaheen@users.noreply.github.com>
2026-08-15 23:11:54 +0600

    Time: 23 ms (49.62%), Space: 14.4 MB (8.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s)!=len(t):
+            return False
+        count_hash_s={}
+        count_hash_t={}
+        for i in range(len(s)):
+            count_hash_s[s[i]]=1+count_hash_s.get(s[i],0)
+            count_hash_t[t[i]]=1+count_hash_t.get(t[i],0)
+        return count_hash_s==count_hash_t
+        
