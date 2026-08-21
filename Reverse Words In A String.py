fd657565a0286944590bed56d3ba09c297bfe4bf
Al Shahriar Bin Shaheen <155906469+shahriarshaheen@users.noreply.github.com>
2026-08-21 14:55:07 +0600

    Delete Reverse Words In A String.py

--- Reverse Words In A String.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def reverseWords(self, s):
-        list_1=s.strip().split()
-        list_2=[]
-        for i in range(len(list_1)-1,-1,-1):
-            list_2.append(list_1[i])
-        return ' '.join(list_2)
-        
