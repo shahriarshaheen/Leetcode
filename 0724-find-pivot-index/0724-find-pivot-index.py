67c99b8894dfe4fcdff8b9e5c3d38075f25c615f
Al Shahriar Bin Shaheen <155906469+shahriarshaheen@users.noreply.github.com>
2026-09-06 12:10:36 +0600

    Time: 12 ms (23.97%), Space: 13.2 MB (87.47%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -3,9 +3,10 @@ class Solution(object):
         total_sum=0
         for i in range(len(nums)):
             total_sum+=nums[i]
+        left_sum=0
         for i in range(len(nums)):
-            left_sum=sum(nums[0:i])
             right_sum=total_sum-left_sum-nums[i]
             if left_sum==right_sum:
                 return i
+            left_sum+=nums[i]
         return -1
