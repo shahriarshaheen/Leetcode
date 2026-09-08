231c6f952c9e1a94b87e80e796763f2a0bb23707
Al Shahriar Bin Shaheen <155906469+shahriarshaheen@users.noreply.github.com>
2026-09-08 15:27:22 +0600

    Time: 0 ms (100%), Space: 12.3 MB (65.05%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def countCommas(self, n):
+        comm_count=n-999
+        if comm_count>0:
+            return comm_count
+        else:
+            return 0
+
+        
